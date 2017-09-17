bd0ea06d79697eb6b0ff208c1329328392b5e354
Hamilton Little <hamilton.little@gmail.com>
2017-09-17 10:48:09 -0700

    usb enumerates as cdc_acm device

---
+++ embedded/knock_lock.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=knock_lock.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/knock_lock.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=knocklock.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/knocklock.x.tar
